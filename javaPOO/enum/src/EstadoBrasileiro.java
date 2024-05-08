e1bb9d783fbb4912a8e0cf4c0f2f49509850ce24
stumpf-alexandre <als.stumpf@gmail.com>
2024-05-08 20:49:06 -0300

    criando um enum

---
+++ javaPOO/enum/src/EstadoBrasileiro.java
@@ -0,0 +1,33 @@
+public enum EstadoBrasileiro{
+    SAO_PAULO ("SP", "São paulo", 11),
+    RIO_JANEIRO ("RJ", "Rio de Janeiro", 12),
+    PIAUI ("PI", "Piauí", 13),
+    MARANHAO ("MA", "Maranhão", 14),
+    CEARA ("CE", "Ceará", 15);
+
+    private String nome;
+    private String sigla;
+    private int ibge;
+
+    private EstadoBrasileiro(String sigla, String nome, int ibge) {
+        this.sigla = sigla;
+        this.nome = nome;
+        this.ibge = ibge;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public int getIbge() {
+        return ibge;
+    }
+
+    public String getNomeMaiusculo(){
+        return nome.toUpperCase();
+    }
+}
