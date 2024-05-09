0dadbca00b9aea5abd54a8cb1cc2c3d2ebe466fb
stumpf-alexandre <als.stumpf@gmail.com>
2024-05-08 23:02:46 -0300

    terminando de extruturar o get e set

--- javaPOO/getters-setters/src/escola/Aluno.java
+++ javaPOO/getters-setters/src/escola/Aluno.java
@@ -4,18 +4,22 @@ public class Aluno {
     private String nome;
     private int idade;
 
+    //metodo get é responsavelpor obter o valor atual do objeto
     public String getNome() {
         return nome;
     }
 
+    //metodo set é responsavel por modificar o valor do objeto
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    //metodo get é responsavelpor obter o valor atual do objeto
     public int getIdade() {
         return idade;
     }
 
+    //metodo set é responsavel por modificar o valor do objeto
     public void setIdade(int idade) {
         this.idade = idade;
     }
