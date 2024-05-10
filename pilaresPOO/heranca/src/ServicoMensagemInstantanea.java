780bd62469c3f97275712d83b1ca6c0951aa4a1f
stumpf-alexandre <als.stumpf@gmail.com>
2024-05-09 23:26:07 -0300

    pilares da POO - herança

---
+++ pilaresPOO/heranca/src/ServicoMensagemInstantanea.java
@@ -0,0 +1,17 @@
+public class ServicoMensagemInstantanea {
+    public void enviarMensagem(){
+        validarConectadoInternet();
+        System.out.println("Enviando mensagem");
+        salvarHistoricoMensagem();
+    }
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem");
+        salvarHistoricoMensagem();
+    }
+    private void validarConectadoInternet(){
+        System.out.println("Validando se está conectado a internet");
+    }
+    private void salvarHistoricoMensagem(){
+        System.out.println("Salvando o histórico da mensagem");
+    }
+}
---
+++ pilaresPOO/heranca/src/Telegram.java
@@ -0,0 +1,1 @@
+public class Telegram extends ServicoMensagemInstantanea{}
