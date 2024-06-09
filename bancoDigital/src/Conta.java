d2200aa221083509f2a04e908689ffc91d65d242
stumpf-alexandre <als.stumpf@gmail.com>
2024-06-09 18:21:46 -0300

    criando uma classe pai conta e fazendo as classes conta corrente e conta poupança suas classes filhas

---
+++ bancoDigital/src/Conta.java
@@ -0,0 +1,32 @@
+public abstract class Conta {
+    //atributo
+    private int agencia;
+    private int conta;
+    private double saldo;
+
+    //metodos
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+        
+    }
+
+    //getters
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
--- bancoDigital/src/ContaPoupanca.java
+++ bancoDigital/src/ContaPoupanca.java
@@ -1,32 +1,3 @@
-public class ContaPoupanca {
-    //atributo
-    private int agencia;
-    private int conta;
-    private double saldo;
-
-    //metodos
-    public void sacar() {
-
-    }
-
-    public void depositar() {
-
-    }
-
-    public void transferir() {
-        
-    }
-
-    //getters
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getConta() {
-        return conta;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }   
+public class ContaPoupanca extends Conta {
+    
 }
