ff3f4fa558ba442f6906f4d98fc12ee8f6465295
stumpf-alexandre <als.stumpf@gmail.com>
2024-07-08 23:10:22 -0300

    Habilitando o tratamento de exceções de negócio com handler

--- springInitializr/myFirstWebApi/src/main/java/dio/web/api/repository/UsuarioRepository.java
+++ springInitializr/myFirstWebApi/src/main/java/dio/web/api/repository/UsuarioRepository.java
@@ -2,13 +2,17 @@ package dio.web.api.repository;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Repository;
+import dio.web.api.handler.CampoObrigatorioException;
 import dio.web.api.model.Usuario;
 
 @Repository
 public class UsuarioRepository {
     public void save(Usuario usuario) {
+        if (usuario.getLogin() == null)
+            throw new CampoObrigatorioException("login");
+        if (usuario.getPassword() == null)
+            throw new CampoObrigatorioException("password");
         if (usuario.getId() == null)
             System.out.println("SAVE - Recebendo o usuário na camada de repositório");
         else
